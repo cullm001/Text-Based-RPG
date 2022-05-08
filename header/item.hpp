9cf6ea4f00c999c3941fea79c129c7314f34a162
Sworddafence <90822210+Sworddafence@users.noreply.github.com>
2022-05-08 16:42:52 -0700

    Updated faulty item.hpp

--- header/item.hpp
+++ header/item.hpp
@@ -2,15 +2,19 @@
 #define ITEM_HPP
 
 #include <string>
+
+using namespace std;
+
 class Item {
     private:
         string name;
+        string description;
         int value;
         int effect;
     public:
-        Item(string n, int v, int e);
+        Item(string n, string d, int v, int e);
         int use();//In the future this use will have the player obejct in it so it can modify it
         void print(); //print the description
 };
 
-#endif // RECTANGLE_HPP
+#endif //blah
