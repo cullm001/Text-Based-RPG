68d20200fcb6708e6d17f62bea8d1cfaaf659dd3
sworddafence <jlee1442@hammer.cs.ucr.edu>
2022-05-15 16:03:02 -0700

    created tests idk why I can't add googletest

---
+++ test/test.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+
+#include "../header/archetypes.hpp"
+#include "../header/item.hpp"
+#include "../header/player.hpp"
+#include "../header/entity.hpp"
+
+
+TEST(Constructer, first) {
+    
+}
+
+
+
+int main(int argc, char **argv) {
+      ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+}
