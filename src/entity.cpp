e5d184418a94c3f5242eb61afaf1d44e5c233a31
Keita Ichii <kichi004@ucr.edu>
2022-05-28 22:49:55 -0700

    fixed bug with defense in a constructor

--- src/entity.cpp
+++ src/entity.cpp
@@ -25,7 +25,7 @@ Entity::Entity(double atk, double def, double hth, int lvl){
     maxHealth = hth;
     currHealth = hth;
     attack = atk;
-    defense = defense;
+    defense = def;
     critRate = 0.15;
     level = lvl;
 }
